919c92d3f36c056b8bd340b210afa3896543d66b
SHAJESH JEGATHEESWARAN <s9jegath@uwaterloo.ca>
2018-01-24 14:58:38 -0500

    Added Sudoku Generator Command

---
+++ ShagBot/Modules/MiscModule.cs
@@ -0,0 +1,38 @@
+﻿using Discord.Commands;
+using ShagBot.Attributes;
+using ShagBot.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShagBot.Modules
+{
+    public class MiscModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("sudoku")]
+        [RequireBotContext(CmdChannelType.BotChannel)]
+        public async Task GenerateSudokuPuzzle()
+        {
+            var sudokuGen = new SudokuGenerator();
+
+            sudokuGen.Create();
+            var puzzleDrawing = sudokuGen.DrawPuzzle();
+
+            var sudokuIdx = 0;
+
+            while (File.Exists($"sudoku{sudokuIdx}.png"))
+            {
+                sudokuIdx++;
+            }
+
+            puzzleDrawing.Save($"sudoku{sudokuIdx}.png");
+
+            await Context.Channel.SendFileAsync($"sudoku{sudokuIdx}.png");
+
+            File.Delete($"sudoku{sudokuIdx}.png");
+        }
+    }
+}
